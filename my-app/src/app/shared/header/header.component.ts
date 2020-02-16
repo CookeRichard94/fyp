17b825bc4efb960d8522b95772b3000265649b73
Richard Cooke <g00331787@gmit.ie>
2020-02-16 14:23:52 +0000

    Added dynamic icon to toolbar

--- my-app/src/app/shared/header/header.component.ts
+++ my-app/src/app/shared/header/header.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   @Input() pageTitle: string;
+  @Input() iconTitle: string;
   constructor() { }
 
   ngOnInit(): void {
