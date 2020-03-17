6c727e78369b58dcc4beb98a0f19507fc02a8b92
Richard Cooke <g00331787@gmit.ie>
2020-03-17 17:48:34 +0000

    Began backend integration

--- my-app/src/app/admin/setproduct/setproduct.component.ts
+++ my-app/src/app/admin/setproduct/setproduct.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+
 
 @Component({
   selector: 'app-setproduct',
@@ -9,12 +13,25 @@ export class SetproductComponent implements OnInit {
 
   toggleField: String; 
   savedChanges = false;
+  error: boolean = false;
+  errorMessage: String = "";
+  dataLoading: boolean = false;
+  private querySubscription;
+  dataSource: MatTableDataSource<any>;
+  members: any[];
   
+  displayedColumns = ['category', 'scategory', 'name', 'price', '_id'];
 
-  constructor() { }
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor(private backendService) { }
 
   ngOnInit(): void {
     this.toggleField = "searchMode";
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+    this.dataSource = new MatTableDataSource(this.members);
   }
 
   toggle(filter?) {
@@ -25,12 +42,28 @@ export class SetproductComponent implements OnInit {
 
   getData()
   {
-
-  }
+    this.dataLoading = true;
+      this.querySubscription = this.backendService.getProducts('product')
+        .subscribe(members => {
+          this.members = members;
+          this.dataSource = new MatTableDataSource(members);
+          this.dataSource.paginator = this.paginator;
+          this.dataSource.sort = this.sort;
+        });
+}
 
   updateDate()
   {
 
+  }
+
+  //data table results
+  applyFilter(filterValue: String){
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if(this.dataSource.paginator){
+      this.dataSource.paginator.firstPage();
+    }
   }
 
   
