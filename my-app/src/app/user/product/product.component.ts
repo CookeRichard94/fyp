3810783d7587ec33f256560f4c331d8d49bc64d1
Richard Cooke <g00331787@gmit.ie>
2020-03-21 15:09:18 +0000

    More work on productDetails

--- my-app/src/app/user/product/product.component.ts
+++ my-app/src/app/user/product/product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BackendService } from '../../Services/backend.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-product',
@@ -8,10 +10,57 @@ import { Component, OnInit } from '@angular/core';
 export class ProductComponent implements OnInit {
 
   toggle : boolean = true;
+  savedChanges = false;
+  error: boolean = false;
+  errorMessage: String = "";
+  dataLoading: boolean = false;
+  private querySubscription;
+  counter = 0;
+  profileUrl: Observable<string | null>;
+  members: Observable<any>;
 
-  constructor() { }
+  constructor(private backend_Service: BackendService) { }
 
   ngOnInit(): void {
+    this.getData();
+  }
+
+  getData()
+  {
+    this.dataLoading = true;
+      this.querySubscription = this.backend_Service.getProducts('product')
+        .subscribe(members => {
+          this.members = members;
+          this.dataLoading = false;
+        },
+        (error) => {
+          this.error  =true;
+          this.errorMessage = error.message;
+          this.dataLoading = false;
+        },
+        () =>{this.error =false; this.dataLoading = false;}
+        );
+  }
+
+  getFilterData(filters)
+  {
+    this.dataLoading = true;
+      this.querySubscription = this.backend_Service.getFilterProducts('product', filters)
+        .subscribe(members => {
+          this.members = members;
+          this.dataLoading = false;
+        },
+        (error) => {
+          this.error  =true;
+          this.errorMessage = error.message;
+          this.dataLoading = false;
+        },
+        () =>{this.error =false; this.dataLoading = false;}
+        );
+  }
+
+  getPic(picID){
+    this.profileUrl = "";
   }
 
 }
