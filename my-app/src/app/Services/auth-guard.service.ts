cf5fe89e5b9159084fe8d04286bd4e13d0271381
Richard Cooke <g00331787@gmit.ie>
2020-03-29 15:08:09 +0100

    Work on authentication guard

--- my-app/src/app/Services/auth-guard.service.ts
+++ my-app/src/app/Services/auth-guard.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import {AngularFireAuth} from '@angular/fire/auth';
+import * as firebase from 'firebase/app';
+import { Observable, from } from "rxjs";
+import 'rxjs/add/operator/do';
+import { take } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
+import { tap } from 'rxjs/operators';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate{
 
-  constructor() { }
+  constructor(public fAuth: AngularFireAuth,  private router: Router) { }
+
+  canActivate(): Observable<boolean>
+  {
+    return from(this.fAuth.authState)
+    .pipe(take(1))
+    .pipe(map(state => !!state))
+    .pipe(tap(authenticated => 
+      {
+        if(!authenticated)
+        {
+          this.router.navigate(['/login']);
+        }
+      }))
+  }
 }
--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -7,14 +7,16 @@ import { SettingsComponent} from './settings/settings.component';
 import { AdmintabComponent } from './admin/admintab/admintab.component';
 import { ProductComponent } from './user/product/product.component';
 import { LoginComponent } from './user/login/login.component';
+import {AuthGuardService} from './Services/auth-guard.service';
+import {AuthGuardAdminService} from './Services/auth-guard-admin.service';
 
 
 const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: '', component: LoginComponent},
-  { path: 'settings', component: SettingsComponent},
-  { path: 'admin', component: AdmintabComponent},
-  { path: 'product', component: ProductComponent},
+  { path: 'settings', component: SettingsComponent, canActivate: [AuthGuardService]},
+  { path: 'admin', component: AdmintabComponent, canActivate: [AuthGuardAdminService]},
+  { path: 'product', component: ProductComponent, canActivate: [AuthGuardService]},
   { path: 'login', component: LoginComponent},
   { path: '**', component: NotFoundComponent}
 ];
