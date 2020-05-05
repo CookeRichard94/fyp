ffba06cf8f14d251459970ec73ace1b2cbe02287
Richard Cooke <g00331787@gmit.ie>
2020-05-05 17:03:10 +0100

    Added comments

--- my-app/src/app/admin/adminusers/adminusers.component.ts
+++ my-app/src/app/admin/adminusers/adminusers.component.ts
@@ -13,6 +13,7 @@ import { BackendService} from '../../Services/backend.service';
 })
 export class AdminusersComponent implements OnInit {
 
+  //Variables
   toggleField: String; 
   savedChanges = false;
   error: boolean = false;
@@ -26,11 +27,13 @@ export class AdminusersComponent implements OnInit {
   takeHostSelfie = false;
   showHostSelfie = false;
   
+  // Used to as a guide for which columns should be part of the mat table
   displayedColumns = ['author', 'firstName', 'surname', 'email'];
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
+  // Importing backendservice
   constructor(private backend_Service: BackendService) { }
 
   ngOnInit(): void {
@@ -41,15 +44,11 @@ export class AdminusersComponent implements OnInit {
     this.getData();
   }
 
-  toggle(filter?) {
-    if(!filter) {filter = "searchMode"}
-    else {filter = filter;}
-    this.toggleField = filter;
-  }
-
+  // Method to retrieve user data from collection
   getData()
   {
     this.dataLoading = true;
+    // Uses backendservice method getproducts to get users, passed the collection name "users" as a parameter
       this.querySubscription = this.backend_Service.getProducts('users')
         .subscribe(members => {
           this.members = members;
@@ -68,7 +67,7 @@ export class AdminusersComponent implements OnInit {
 
   
 
-  //data table results
+  //Method to filter the data available in mat table
   applyFilter(filterValue: String){
     this.dataSource.filter = filterValue.trim().toLowerCase();
 
--- my-app/src/app/admin/setproduct/setproduct.component.ts
+++ my-app/src/app/admin/setproduct/setproduct.component.ts
@@ -15,6 +15,7 @@ import { AngularFireStorage } from '@angular/fire/storage';
 })
 export class SetproductComponent implements OnInit, OnDestroy {
 
+  // Variables
   toggleField: String; 
   savedChanges = false;
   error: boolean = false;
@@ -29,29 +30,35 @@ export class SetproductComponent implements OnInit, OnDestroy {
   showHostSelfie = false;
   profileUrl: Observable<string | null>;
   
+  // Columns to outline the tables heads in the mat table
   displayedColumns = ['category', 'name', 'price', '_id'];
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
+  //Import angular storage and the backend service
   constructor(private backend_Service: BackendService, private storage: AngularFireStorage) { }
 
   ngOnInit(): void {
+    // Initially set to searchmode
     this.toggleField = "searchMode";
     this.dataSource = new MatTableDataSource(this.members);
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
   }
 
+  // Toggle method to allow for different templates to be activated
   toggle(filter?) {
     if(!filter) {filter = "searchMode"}
     else {filter = filter;}
     this.toggleField = filter;
   }
 
+  //Retrieves product data from the database
   getData()
   {
     this.dataLoading = true;
+    //Uses getproducts method and passes parameter "product"
     this.querySubscription = this.backend_Service.getProducts('product')
         .subscribe(members => {
             this.members = members;
@@ -61,9 +68,11 @@ export class SetproductComponent implements OnInit, OnDestroy {
         });
   }
 
+  // Creates a new product and passes the data in the creation form to the database
   setData(formData)
   {
     this.dataLoading = true;
+    // Passes the "product" to sigal which collection to send to and the from data to fille out the document
       this.querySubscription = this.backend_Service.setProducts('product',formData)
         .then((res) => {
           this.savedChanges =true;
@@ -77,9 +86,11 @@ export class SetproductComponent implements OnInit, OnDestroy {
   }
         
 
+  // Updates a current product
   updateData(formData)
   {
     this.dataLoading = true;
+    // Works exactly like setData except passes data to an existing document
       this.querySubscription = this.backend_Service.updateProducts('product',formData)
       .then((res) => {
         this.savedChanges =true;
@@ -94,6 +105,7 @@ export class SetproductComponent implements OnInit, OnDestroy {
   }
 
   //data table results
+  // Filters through product data
   applyFilter(filterValue: String){
     this.dataSource.filter = filterValue.trim().toLowerCase();
 
@@ -102,9 +114,11 @@ export class SetproductComponent implements OnInit, OnDestroy {
     }
   }
 
+  // Allows for search of specific product
   getFilterData(filters)
   {
     this.dataLoading = true;
+    // Search product collection using getfilterproduct method and passes entered filters to narrow results
       this.querySubscription = this.backend_Service.getFilterProducts('product', filters)
         .subscribe(members => {
           this.members = members;
@@ -121,13 +135,17 @@ export class SetproductComponent implements OnInit, OnDestroy {
         );
   }
 
+  // Returns a single document instead of all, uses the docId to identify the product, used for editing existing product
   getDoc(docId)
   {
+
     this.dataLoading = true;
+    // search product collection by the id of the document you want
       this.querySubscription = this.backend_Service.getProductDoc('product', docId)
         .subscribe(res => {
          if(res)
          {
+           // Change to edit mode if it exists on collection
           this.myDocData = res;
           this.toggle('editMode');
          }
@@ -141,21 +159,27 @@ export class SetproductComponent implements OnInit, OnDestroy {
         );
   }
 
+  // Method to delete a product
   deleteDoc(docId)
   {
+    //Uses a confirm to ascertain if the admin wants to remove product
     if (confirm("Are you sure want to delete this product ?")) {
       this.dataLoading = true;
+      //Passes the id of the document to specify particular document
       this.backend_Service.deleteProductDoc('product', docId).then((res) => {
+        //switches to search mode
           this.toggle('searchMode');
       });
   }
   }
 
+  // retrives picture from collection and passes its path to variable
   getPic(picId) {
     const ref = this.storage.ref(picId);
     this.profileUrl = ref.getDownloadURL();
   }
   
+  // Deletes specified picture, contains a confirm to ascertain the decision should take place
   deleteProductPic(docId){
     if (confirm("Are you sure want to delete this picture ?")) {
     this.backend_Service.deleteProductPic('product',docId);
